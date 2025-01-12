0494ac631fadd8399f49243617b0344ab019df54
ssal6113 <ssal6113@uni.sydney.edu.au>
2025-01-12 17:00:19 +0600

    first push

--- index.js
+++ index.js
@@ -2,14 +2,35 @@ import express from "express"
 import bodyParser from "body-parser";
 
 
-const app = express()
 
+var posts = []
+
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {blog_posts: posts});
 });
 
 app.post("/post", (req, res) => {
-    console.log("Post request received");
+    var content = {
+        title: req.body.title,
+        content: req.body.content
+    }
+    console.log("here goes a push!")
+    posts.push(content);
+    res.redirect("/")
+    
+
+});
+
+
+app.post("/delete/:index", (req, res) => {
+    const index = parseInt(req.params.index); // Get and parse index
+    if (index >= 0 && index < posts.length) {
+        console.log(`Deleting post at index ${index}`);
+        posts.splice(index, 1); // Remove the post at the specified index
+    }
+    res.redirect("/"); // Redirect back to home page after deletion
 });
 
 app.listen(3000, () => {
